556545932341a42a8d657784b0b9254de0c8eee6
Kbuczynski <kamil.buczynski00@interia.pl>
2020-06-21 01:15:51 +0200

    Updates

---
+++ precache-manifest.d82605f6c4880099c149f8bed42fdf7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb2fae38eb816ff7d68190849bb798c",
+    "url": "/gifs-search-app/index.html"
+  },
+  {
+    "revision": "1294dade1beeaf0faef4",
+    "url": "/gifs-search-app/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "e0201581b438f28fd742",
+    "url": "/gifs-search-app/static/css/main.3ca7ca56.chunk.css"
+  },
+  {
+    "revision": "1294dade1beeaf0faef4",
+    "url": "/gifs-search-app/static/js/2.fde5d0d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gifs-search-app/static/js/2.fde5d0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0201581b438f28fd742",
+    "url": "/gifs-search-app/static/js/main.95f7f4c0.chunk.js"
+  },
+  {
+    "revision": "ae1d230313981608eb49",
+    "url": "/gifs-search-app/static/js/runtime-main.1ceef42c.js"
+  }
+]);
